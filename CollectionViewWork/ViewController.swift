646510f8f876227c3f06db0df80c47a8fc7a37d0
Merve Dönmez <merve.donmez@ibtech.com.tr>
2023-02-19 13:01:58 +0300

    Initial Commit

---
+++ CollectionViewWork/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  CollectionViewWork
+//
+//  Created by C02QJ4LQG8WN on 2/19/23.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
